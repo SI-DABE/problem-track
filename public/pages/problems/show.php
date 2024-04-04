053e32ba6833f7f19c9cc89fc34a8351441aa84b
Prof. Diego Marczal <37582258+MarczalTSIGP@users.noreply.github.com>
2024-04-04 15:30:19 -0300

    Retrive problems details (#8)
    Co-authored-by: Diego Marczal <dmarczal@gmail.com>

--- public/pages/problems/show.php
+++ public/pages/problems/show.php
@@ -1,10 +1,12 @@
 <?php
+$id = intval($_GET['id']);
 
 define('DB_PATH', '/var/www/database/problems.txt');
-
 $problems = file(DB_PATH, FILE_IGNORE_NEW_LINES);
 
-$title = 'Problemas Registrados';
-$view = '/var/www/app/views/problems/index.phtml';
+$problem['title'] = $problems[$id];
+
+$title = "Visualização do Problema #{$id}";
+$view = '/var/www/app/views/problems/show.phtml';
 
 require '/var/www/app/views/layouts/application.phtml';
