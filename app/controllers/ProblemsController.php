a040b142ea588a9bc67953f3befca3c89c86f475
Diego Marczal <dmarczal@gmail.com>
2024-04-08 07:55:35 -0300

    Refactor pages to controllers using OO

---
+++ app/controllers/ProblemsController.php
@@ -0,0 +1,105 @@
+<?php
+
+require '/var/www/app/models/Problem.php';
+
+class ProblemsController
+{
+    private string $layout = 'application';
+
+    public function index()
+    {
+        $problems = Problem::all();
+
+        $title = 'Problemas Registrados';
+        $this->render('index', compact('problems', 'title'));
+    }
+
+    public function show()
+    {
+        $id = intval($_GET['id']);
+
+        $problem = Problem::findById($id);
+
+        $title = "Visualização do Problema #{$id}";
+        $this->render('show', compact('problem', 'title'));
+    }
+
+    public function new()
+    {
+        $problem = new Problem();
+
+        $title = 'Novo Problema';
+        $this->render('new', compact('problem', 'title'));
+    }
+
+    public function create()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        if ($method !== 'POST') $this->redirectTo('/pages/problems');
+
+        $params = $_POST['problem'];
+        $problem = new Problem(title: $params['title']);
+
+        if ($problem->save()) {
+            $this->redirectTo('/pages/problems');
+        } else {
+            $title = 'Novo Problema';
+
+            $this->render('new', compact('problem', 'title'));
+        }
+    }
+
+    public function edit()
+    {
+        $id = intval($_GET['id']);
+
+        $problem = Problem::findById($id);
+
+        $title = "Editar Problema #{$id}";
+        $this->render('edit', compact('problem', 'title'));
+    }
+
+    public function update()
+    {
+        $method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+        if ($method !== 'PUT') $this->redirectTo('/pages/problems');
+
+        $params = $_POST['problem'];
+
+        $problem = Problem::findById($params['id']);
+        $problem->setTitle($params['title']);
+
+        if ($problem->save()) {
+            $this->redirectTo('/pages/problems');
+        } else {
+            $title = "Editar Problema #{$problem->getId()}";
+            $this->render('edit', compact('problem', 'title'));
+        }
+    }
+
+    public function destroy()
+    {
+        $method = $_REQUEST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+        if ($method !== 'DELETE') $this->redirectTo('/pages/problems');
+
+        $problem = Problem::findById($_POST['problem']['id']);
+        $problem->destroy();
+
+        $this->redirectTo('/pages/problems');
+    }
+
+
+    private function render($view, $data = [])
+    {
+        extract($data);
+
+        $view = '/var/www/app/views/problems/' . $view . '.phtml';
+        require '/var/www/app/views/layouts/' . $this->layout . '.phtml';
+    }
+
+    private function redirectTo($location)
+    {
+        header('Location: ' . $location);
+        exit;
+    }
+}
--- public/pages/problems/edit.php
+++ public/pages/problems/edit.php
@@ -1,11 +1,5 @@
 <?php
-require '/var/www/app/models/Problem.php';
+require '/var/www/app/controllers/ProblemsController.php';
 
-$id = intval($_GET['id']);
-
-$problem = Problem::findById($id);
-
-$title = "Editar Problema #{$id}";
-$view = '/var/www/app/views/problems/edit.phtml';
-
-require '/var/www/app/views/layouts/application.phtml';
+$controller = new ProblemsController();
+$controller->edit();
