d3d843ff2079b1b876bac541ea85f70803828f4c
Diego Marczal <dmarczal@gmail.com>
2024-04-16 15:52:47 -0300

    Refactor errors handler to OO

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,14 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use Core\Errors\ErrorsHandler;
+
+ErrorsHandler::init();
+
+
 require_once __DIR__ . '/../core/constants/general.php';
 
 require_once ROOT_PATH . '/core/env/env.php';
 require_once ROOT_PATH . '/core/debug/functions.php';
-require_once ROOT_PATH . '/core/errors/handler.php';
-
-require __DIR__ . '/../vendor/autoload.php';
+// require_once ROOT_PATH . '/core/errors/handler.php';
