6df63b3d345eb171530bcd9755625ec013068c94
Diego Marczal <dmarczal@gmail.com>
2024-04-28 14:43:01 -0300

    Enhancement dump function

--- core/Debug/Debugger.php
+++ core/Debug/Debugger.php
@@ -6,7 +6,20 @@ class Debugger
 {
     public static function dd(): void
     {
-        var_dump(func_get_args());
+        $str = '';
+        foreach (func_get_args() as $index => $value) {
+            if ($index !== 0) $str .= '<hr>';
+
+            $str .= highlight_string('<?php ' . self::dump($value) . '?>', true);
+        }
+        echo str_replace(['&lt;?php', '?&gt;'], '', $str);
         exit;
+    }
+
+    private static function dump($value)
+    {
+        ob_start();
+        var_dump($value);
+        return ob_get_clean();
     }
 }
