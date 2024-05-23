e2e176bfc2e5ce49ba30fe0dd7c732d8a04ed6d3
Diego Marczal <dmarczal@gmail.com>
2024-05-23 00:34:22 -0300

    Refactor problem test

--- tests/Unit/Models/ProblemTest.php
+++ tests/Unit/Models/ProblemTest.php
@@ -9,7 +9,7 @@ class ProblemTest extends TestCase
 {
     public function test_should_create_new_problem(): void
     {
-        $problem = new Problem(title: 'Problem 1');
+        $problem = new Problem(['title' => 'Problem 1']);
 
         $this->assertTrue($problem->save());
         $this->assertCount(1, Problem::all());
@@ -17,8 +17,8 @@ class ProblemTest extends TestCase
 
     public function test_all_should_return_all_problems(): void
     {
-        $problems[] = new Problem(title: 'Problem 1');
-        $problems[] = new Problem(title: 'Problem 2');
+        $problems[] = new Problem(['title' => 'Problem 1']);
+        $problems[] = new Problem(['title' => 'Problem 2']);
 
         foreach ($problems as $problem) {
             $problem->save();
@@ -31,8 +31,8 @@ class ProblemTest extends TestCase
 
     public function test_destroy_should_remove_the_problem(): void
     {
-        $problem1 = new Problem(title: 'Problem 1');
-        $problem2 = new Problem(title: 'Problem 2');
+        $problem1 = new Problem(['title' => 'Problem 1']);
+        $problem2 = new Problem(['title' => 'Problem 2']);
 
         $problem1->save();
         $problem2->save();
@@ -43,22 +43,22 @@ class ProblemTest extends TestCase
 
     public function test_set_title(): void
     {
-        $problem = new Problem(title: 'Problem 1');
-        $this->assertEquals('Problem 1', $problem->getTitle());
+        $problem = new Problem(['title' => 'Problem 1']);
+        $this->assertEquals('Problem 1', $problem->title);
     }
 
     public function test_set_id(): void
     {
-        $problem = new Problem(title: 'Problem Test');
-        $problem->setId(7);
+        $problem = new Problem(['title' => 'Problem 1']);
+        $problem->id = 7;
 
-        $this->assertEquals(7, $problem->getId());
+        $this->assertEquals(7, $problem->id);
     }
 
     public function test_errors_should_return_title_error(): void
     {
-        $problem = new Problem(title: 'Problem 1');
-        $problem->setTitle('');
+        $problem = new Problem(['title' => 'Problem 1']);
+        $problem->title = '';
 
         $this->assertFalse($problem->isValid());
         $this->assertFalse($problem->save());
@@ -69,20 +69,20 @@ class ProblemTest extends TestCase
 
     public function test_find_by_id_should_return_the_problem(): void
     {
-        $problem2 = new Problem(title: 'Problem 2');
-        $problem1 = new Problem(title: 'Problem 1');
-        $problem3 = new Problem(title: 'Problem 3');
+        $problem2 = new Problem(['title' => 'Problem 2']);
+        $problem1 = new Problem(['title' => 'Problem 1']);
+        $problem3 = new Problem(['title' => 'Problem 3']);
 
         $problem1->save();
         $problem2->save();
         $problem3->save();
 
-        $this->assertEquals($problem1, Problem::findById($problem1->getId()));
+        $this->assertEquals($problem1, Problem::findById($problem1->id));
     }
 
     public function test_find_by_id_should_return_null(): void
     {
-        $problem = new Problem(title: 'Problem 1');
+        $problem = new Problem(['title' => 'Problem 1']);
         $problem->save();
 
         $this->assertNull(Problem::findById(7));
