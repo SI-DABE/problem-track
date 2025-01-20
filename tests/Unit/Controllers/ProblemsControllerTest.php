6b6d2d6965a57abc2541d3061500b82429fc8b03
Diego Marczal <dmarczal@gmail.com>
2025-01-20 18:26:43 -0300

    Added controller tests to ProblemController

--- tests/Unit/Controllers/ProblemsControllerTest.php
+++ tests/Unit/Controllers/ProblemsControllerTest.php
@@ -7,28 +7,123 @@ use App\Models\User;
 
 class ProblemsControllerTest extends ControllerTestCase
 {
-    public function test_list_all_problems(): void
+    private User $user;
+
+    public function setUp(): void
     {
-        $user = new User([
+        parent::setUp();
+        $this->user = new User([
             'name' => 'User 1',
             'email' => 'fulano@example.com',
             'password' => '123456',
             'password_confirmation' => '123456'
         ]);
-        $user->save();
-        $_SESSION['user']['id'] = $user->id;
+        $this->user->save();
+        $_SESSION['user']['id'] = $this->user->id;
+    }
 
-        $problems[] = new Problem(['title' => 'Problem 1', 'user_id' => $user->id]);
-        $problems[] = new Problem(['title' => 'Problem 2',  'user_id' => $user->id]);
+    public function test_list_all_problems(): void
+    {
+        $problems[] = new Problem(['title' => 'Problem 1', 'user_id' => $this->user->id]);
+        $problems[] = new Problem(['title' => 'Problem 2',  'user_id' => $this->user->id]);
 
         foreach ($problems as $problem) {
             $problem->save();
         }
 
-        $response = $this->get(action: 'index', controller: 'App\Controllers\ProblemsController');
+        $response = $this->get(action: 'index', controllerName: 'App\Controllers\ProblemsController');
 
         foreach ($problems as $problem) {
             $this->assertMatchesRegularExpression("/{$problem->title}/", $response);
         }
+    }
+
+    public function test_show_problem(): void
+    {
+        $problem = new Problem(['title' => 'Problem 1', 'user_id' => $this->user->id]);
+        $problem->save();
+
+        $response = $this->get(
+            action: 'show',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: ['id' => $problem->id]
+        );
+
+        $this->assertMatchesRegularExpression("/Visualização do Problema #{$problem->id}/", $response);
+        $this->assertMatchesRegularExpression("/{$problem->title}/", $response);
+    }
+
+    public function test_successfully_create_problem(): void
+    {
+        $params = ['problem' => ['title' => 'Problema test']];
+
+        $response = $this->post(
+            action: 'create',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: $params
+        );
+
+        $this->assertMatchesRegularExpression("/Location: \/problems/", $response);
+    }
+
+    public function test_unsuccessfully_create_problem(): void
+    {
+        $params = ['problem' => ['title' => '']];
+
+        $response = $this->post(
+            action: 'create',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: $params
+        );
+
+        $this->assertMatchesRegularExpression("/não pode ser vazio!/", $response);
+    }
+
+    public function test_edit_problem(): void
+    {
+        $problem = new Problem(['title' => 'Problem 1', 'user_id' => $this->user->id]);
+        $problem->save();
+
+        $response = $this->get(
+            action: 'edit',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: ['id' => $problem->id]
+        );
+
+        $this->assertMatchesRegularExpression("/Editar Problema #{$problem->id}/", $response);
+
+        $regex = '/<input\s[^>]*type=[\'"]text[\'"][^>]*name=[\'"]problem\[title\][\'"][^>]*value=[\'"]Problem 1[\'"][^>]*>/i';
+        $this->assertMatchesRegularExpression($regex, $response);
+    }
+
+
+    public function test_successfully_update_problem(): void
+    {
+        $problem = new Problem(['title' => 'Problem 1', 'user_id' => $this->user->id]);
+        $problem->save();
+        $params = ['id' => $problem->id, 'problem' => ['title' => 'Problem updated']];
+
+        $response = $this->put(
+            action: 'update',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: $params
+        );
+
+        $this->assertMatchesRegularExpression("/Location: \/problems/", $response);
+    }
+
+    public function test_unsuccessfully_update_problem(): void
+    {
+        $problem = new Problem(['title' => 'Problem 1', 'user_id' => $this->user->id]);
+        $problem->save();
+        $params = ['id' => $problem->id, 'problem' => ['title' => '']];
+
+        $response = $this->put(
+            action: 'update',
+            controllerName: 'App\Controllers\ProblemsController',
+            params: $params
+        );
+
+        $this->assertMatchesRegularExpression("/não pode ser vazio!/", $response);
     }
 }
